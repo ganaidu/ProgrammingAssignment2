d98dde92d8810b7150e3887f48aa59869e935487
data_science <gandian@kl-13673.kaust.edu.sa>
2017-01-29 22:16:41 +0300

    solution1

--- cachematrix.R
+++ cachematrix.R
@@ -6,20 +6,19 @@
 makeCacheMatrix <- function(x = matrix( numeric() )) {
         # creates a new matrix for  the inverse.
         m <- matrix(numeric()  ,ncol=ncol(x), nrow=nrow(x))
-
         # creates a set function for setting a new matrix and its inverse  
         set <- function(y) {
                 x <<- y
-                m <<- matrix(numeric()  ,ncol = ncol(x), nrow = nrow(x))  
+                m <<- matrix(numeric() ,ncol = ncol(x), nrow = nrow(x))  
         }
 
-        # Read the matrix
+        # Reads the matrix
         get <- function() x
         # function for setting the inverse
         setinv <- function(solve) m <<- solve
         # function for getting the inverse    
         getinv <- function() m
-        # list returning functions 
+        # list returns functions 
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
@@ -31,7 +30,7 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         # getting the inverse matrix from the getinverse function
         m <- x$getinv()
-        #Checking if the inverse matrix is set or not. Returns inverse if available in the catche.
+        #Checking if the inverse matrix is set or not. Returns inverse if available in the cache.
         if(!all(is.na(m))) {
                 message("getting cached data")
                 return(m)
@@ -42,7 +41,7 @@ cacheSolve <- function(x, ...) {
         m <- solve(data)
         # setting the inverse in the setinv function.
         x$setinv(m)
-        # Return inverse
+        # Returns inverse
         m
 
 }
